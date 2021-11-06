0ce08308034fe8e0d51fe0a5b819fea4fc3cd9c0
Blackjackdeath <blackjack.death@gmail.com>
2021-11-06 16:43:01 +0200

    do invers polish form

---
+++ script/main.js
@@ -0,0 +1,193 @@
+class expressionCalc {
+    constructor() {
+        this.observers = [];
+    }
+    watch(observer) {
+        this.observers.push(observer);
+    }
+    clear(observer) {
+        this.observers = this.observers.filter(obs => obs !== observer);
+    }
+    equal(expression) {
+        this.observers.forEach(observer => {
+            observer.doPolish(expression);
+        })
+    }
+}
+class Observer {
+    constructor() {
+        this.result = 0;
+        this.outArr = [];
+        this.steck = [];
+        this.lowPriorityOperand = ['+', '-', '('];
+        this.heightPriorityOperand = ['*', '÷'];
+    }
+    doPolish(expression) {
+        let i = 0;
+        let number = '';
+        while (expression.length > 0) {
+            if (!Number.isNaN(parseInt(expression[i]))) {
+                while (true) {
+                    if (!Number.isNaN(parseInt(expression[i])) || expression[i] === '.') {
+                        number += expression[i];
+                        i++;
+                    }
+                    else break;
+                };
+                this.outArr.push(parseFloat(number));
+                expression = expression.slice(number.length);
+                i = 0;
+                number = '';
+            }
+            else {
+                if (this.steck.length === 0) {
+                    this.steck.push(expression[i]);
+                    expression = expression.slice(1);
+                }
+                else {
+                    if (expression[i] === '(') {
+                        this.steck.push(expression[i]);
+                        expression = expression.slice(1);
+                    };
+                    if (expression[i] === ')') {
+                        let j = this.steck.length - 1;
+                        while (this.steck[j] !== '(') {
+                            this.outArr.push(this.steck[j]);
+                            j--;
+                        }
+                        this.steck.length = j;
+                        expression = expression.slice(1);
+                    }
+                    if (this.lowPriorityOperand.includes(expression[i]) && this.lowPriorityOperand.includes(this.steck[this.steck.length - 1])) {
+                        if (this.steck[this.steck.length - 1] === '(') {
+                            this.steck.push(expression[i]);
+                            expression = expression.slice(1);
+                        }
+                        else {
+                            this.outArr.push(this.steck[this.steck.length - 1]);
+                            this.steck[this.steck.length - 1] = expression[i];
+                            expression = expression.slice(1);
+                        }
+
+                    };
+                    if (this.heightPriorityOperand.includes(expression[i]) && this.lowPriorityOperand.includes(this.steck[this.steck.length - 1])) {
+                        this.steck.push(expression[i]);
+                        expression = expression.slice(1);
+                    };
+                    if (this.heightPriorityOperand.includes(expression[i]) && this.heightPriorityOperand.includes(this.steck[this.steck.length - 1])) {
+                        this.outArr.push(this.steck[this.steck.length - 1]);
+                        this.steck[this.steck.length - 1] = expression[i];
+                        expression = expression.slice(1);
+                    }
+                    if (this.lowPriorityOperand.includes(expression[i]) && this.heightPriorityOperand.includes(this.steck[this.steck.length - 1])) {
+                        this.outArr.push(this.steck[this.steck.length - 1]);
+                        this.steck[this.steck.length - 1] = expression[i];
+                        expression = expression.slice(1);
+                    };
+                }
+            }
+        }
+        this.steck.reverse();
+        this.outArr = this.outArr.concat(this.steck);
+        this.steck = [];
+        console.log(this.outArr);
+        this.outArr = [];
+    };
+    doCalck(polishExpression) {
+
+    };
+
+}
+
+const calculator = (function () {
+    let buttonCalc = document.querySelectorAll('[number]');
+    let previos = document.querySelector('.calcDisplay__previousOperand');
+    let curent = document.querySelector('.calcDisplay__curentOperand');
+    let ac = document.querySelector('[ac]');
+    let del = document.querySelector('[del]');
+    let point = document.querySelector('[point]');
+    let buttonOperand = document.querySelectorAll('[operand]');
+    let checkOperand = true;
+    let bracketOpen = document.querySelector('[bracketOpen]');
+    let bracketClose = document.querySelector('[bracketClose]');
+    let bracketOpenCheck = false;
+    let curentBracket = 0;
+    let operandSqr = document.querySelector('[sqr]');
+    let operandSqrt = document.querySelector('[sqrt]');
+    const calc1 = new expressionCalc;
+    let equal = document.querySelector('[equal]');
+    const obs = new Observer;
+    function enterPoint() {
+        if (curent.textContent === '') return;
+        if (curent.textContent.includes('.')) return
+        curent.textContent += this.textContent;
+    }
+    function enterNumber() {
+        curent.textContent += this.textContent;
+        checkOperand = true;
+        bracketOpenCheck = false;
+    };
+    function allClear() {
+        curent.textContent = '';
+        previos.textContent = '';
+    };
+    function deleteNumber() {
+        curent.textContent = curent.textContent.substr(0, curent.textContent.length - 1);
+    };
+    function enterOperand() {
+        if (previos.textContent === '' && curent.textContent === '') return;
+        if (bracketOpenCheck) {
+            previos.textContent = previos.textContent + '0';
+        }
+        if (checkOperand) {
+            previos.textContent = previos.textContent + curent.textContent + this.textContent;
+            curent.textContent = '';
+            checkOperand = false;
+            bracketOpenCheck = false;
+        }
+        else {
+            previos.textContent = previos.textContent.substr(0, previos.textContent.length - 1) + this.textContent;
+        }
+
+    };
+    function bracketOpenFunction() {
+        previos.textContent = previos.textContent + '(';
+        bracketOpenCheck = true;
+        checkOperand = true;
+        curentBracket++;
+    };
+    function bracketCloseFunction() {
+        if (curentBracket === 0) return;
+        curentBracket--;
+        if (!checkOperand || previos.textContent[previos.textContent.length - 1] === '(') {
+            previos.textContent = previos.textContent + '0)';
+            checkOperand = true;
+        }
+        else {
+            previos.textContent = previos.textContent + curent.textContent + ')';
+            curent.textContent = '';
+        }
+    };
+    function equalFunction() {
+        previos.textContent = previos.textContent + curent.textContent;
+        calc1.watch(obs);
+        calc1.equal(previos.textContent);
+        calc1.clear(obs);
+    }
+    function bindEvent() {
+        buttonCalc.forEach(element => element.addEventListener('click', enterNumber));
+        ac.addEventListener('click', allClear);
+        del.addEventListener('click', deleteNumber);
+        point.addEventListener('click', enterPoint);
+        buttonOperand.forEach(element => element.addEventListener('click', enterOperand));
+        bracketOpen.addEventListener('click', bracketOpenFunction);
+        bracketClose.addEventListener('click', bracketCloseFunction);
+        equal.addEventListener('click', equalFunction)
+    };
+
+    return {
+        bindEvent: bindEvent
+    }
+})();
+
+calculator.bindEvent();
